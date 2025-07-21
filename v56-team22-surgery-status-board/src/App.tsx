f6b3b452b0fc7296d5360bddda8bab90a9e056eb
Manpreet Singh <ManpreetSL@users.noreply.github.com>
2025-07-21 22:26:30 +0100

    feat: scaffold a sign in screen & link to it from the home screen

--- v56-team22-surgery-status-board/src/App.tsx
+++ v56-team22-surgery-status-board/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from './components/ui/button';
+import { Link } from 'react-router';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,6 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Link to="/sign-in">
+        <Button>Sign in</Button>
+      </Link>
     </>
   );
 }
--- v56-team22-surgery-status-board/src/main.tsx
+++ v56-team22-surgery-status-board/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router';
+import SignIn from './screens/SignIn.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+  },
+  {
+    path: '/sign-in',
+    Component: SignIn,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
