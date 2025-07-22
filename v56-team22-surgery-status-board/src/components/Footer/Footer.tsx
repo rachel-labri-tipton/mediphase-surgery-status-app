e5307d2f375ed5aac319231a0951c91f9ebed907
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-07-22 15:05:12 +0100

    feat: add MainLayout and Footer components, refactor App component

---
+++ v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="w-full px-6 bg-[hsl(82,84.5%,67.1%)] flex justify-between items-center">
+      <div>LOGO</div>
+      <div >
+        <h3 className="font-bold ">Developers</h3>
+        <div className="flex flex-col">
+          <div>Mohamed</div>
+          <div>Jane</div>
+          <div>Ali</div>
+        </div>
+      </div>
+      <div>Pages</div>
+    </footer>
+  );
+};
+
+export default Footer;
