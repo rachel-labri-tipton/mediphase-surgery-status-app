8ef5dc85407574b9d1757d3142d41804947b5e2b
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-07-23 14:36:22 +0100

    fix: correct HTML elements in Footer component for better semantics and styling

--- v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
+++ v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
@@ -1,21 +1,21 @@
 const Footer = () => {
   return (
-    <footer className="w-full px-6 bg-[hsl(82,84.5%,67.1%)] flex flex-col justify-between items-center">
+    <footer className="w-full px-6  bg-[hsl(82,84.5%,67.1%)] flex flex-col justify-between items-center">
       <div className="flex justify-between items-center w-full">
         <div>LOGO</div>
         <div className="my-1">
           <h1 className=" px-4 my-1 font-semibold ">Developers</h1>
           <div className="flex flex-col border-1 rounded-2xl gap-2 px-4 py-2 font-mono">
-            <div>Avtar Singh</div>
-            <div>Mohamed Ouederni</div>
-            <div>Rachel Tipton</div>
-            <div>Rapha</div>
+            <span>Avtar Singh</span>
+            <span>Mohamed Ouederni</span>
+            <span>Rachel Tipton</span>
+            <span>Rapha</span>
           </div>
         </div>
         <div>Pages</div>
       </div>
       <div>
-        <h1 className="px-4 my-1 font-semibold">
+        <h1 className="px-4 my-5 font-semibold">
           <a
             href="https://github.com/chingu-voyages/V56-tier2-team-22"
             target="_blank"
