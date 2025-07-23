7f75d3cde6a0678cb74ca122fbbea8f20f7cebd6
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-07-23 13:41:19 +0100

    feat: update Footer component with team members and GitHub link; clean up App component imports

--- v56-team22-surgery-status-board/src/App.tsx
+++ v56-team22-surgery-status-board/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import { Button } from './components/ui/button';
 import { Link } from 'react-router';
 import MainLayout from './layout/MainLayout';
--- v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
+++ v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
@@ -1,16 +1,29 @@
 const Footer = () => {
   return (
-    <footer className="w-full px-6 bg-[hsl(82,84.5%,67.1%)] flex justify-between items-center">
-      <div>LOGO</div>
-      <div >
-        <h3 className="font-bold ">Developers</h3>
-        <div className="flex flex-col">
-          <div>Mohamed</div>
-          <div>Jane</div>
-          <div>Ali</div>
+    <footer className="w-full px-6 bg-[hsl(82,84.5%,67.1%)] flex flex-col justify-between items-center">
+      <div className="flex justify-between items-center w-full">
+        <div>LOGO</div>
+        <div className="my-1">
+          <h1 className=" px-4 my-1 font-semibold ">Developers</h1>
+          <div className="flex flex-col border-1 rounded-2xl gap-2 px-4 py-2 font-mono">
+            <div>Avtar Singh</div>
+            <div>Mohamed Ouederni</div>
+            <div>Rachel Tipton</div>
+            <div>Rapha</div>
+          </div>
         </div>
+        <div>Pages</div>
       </div>
-      <div>Pages</div>
+      <div>
+        <h1 className="px-4 my-1 font-semibold">
+          <a
+            href="https://github.com/chingu-voyages/V56-tier2-team-22"
+            target="_blank"
+          >
+            Github Project Link
+          </a>
+        </h1>
+      </div>
     </footer>
   );
 };
