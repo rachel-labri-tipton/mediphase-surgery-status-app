99233c7fff1c48dcc86ecc61aa611ac7ceb89c2a
Rapha Chris <raphachris144@gmail.com>
2025-07-26 15:37:37 +0100

    created dummy data, update hompage, update readme

--- v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
+++ v56-team22-surgery-status-board/src/components/Footer/Footer.tsx
@@ -19,7 +19,9 @@ const devNames = [
     github: 'https://github.com/rachel-labri-tipton',
     linkedin: 'https://www.linkedin.com/in/rachel-labri-tipton',
   },
-  { name: 'Rapha', github: 'https://github.com/Excalibur097', linkedin: '' },
+  { name: 'Rapha', 
+  github: 'https://github.com/Excalibur097',
+  linkedin: 'https://www.linkedin.com/in/sugari-rapha-8823a58a/' },
 ];
 
 const pages = [
