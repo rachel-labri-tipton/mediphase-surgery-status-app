f2c8dc8e53355c60a9dfc22bde514a16aea8b853
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-08 18:09:23 +0200

    remove mainlayout component from App.tsx file

--- v56-team22-surgery-status-board/src/App.tsx
+++ v56-team22-surgery-status-board/src/App.tsx
@@ -1,11 +1,8 @@
-import MainLayout from './layout/MainLayout';
 import  Home  from './screens/Home';
 
 function App() {
   return (
-      <MainLayout>
         <Home/>
-      </MainLayout>
   );
 }
 
