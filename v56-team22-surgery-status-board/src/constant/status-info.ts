551b1032c0875e6c40d1dcb4d297515b76666065
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-08 17:14:00 +0200

    add status-info constant that provides constants for the status colors and messages

---
+++ v56-team22-surgery-status-board/src/constant/status-info.ts
@@ -0,0 +1,54 @@
+export type StatusData = {status: string;
+  message: string;
+  color: string;
+};
+
+export const statusLabels = [
+  'Checked In',
+  'Pre-Procedure',
+  'In-Progress',
+  'Closing',
+  'Recovery',
+  'Complete',
+  'Dismissal',
+] 
+
+
+export const statusInfo: StatusData[] =
+  [
+  {
+    "status": "Checked In",
+    "message": "In the facility awaiting their procedure.",
+    "color": "#0077CC"
+  },
+  {
+    "status": "Pre-Procedure",
+    "message": "Undergoing surgical preparation.",
+    "color": "#FFC107"
+  },
+  {
+    "status": "In-Progress",
+    "message": "Surgical procedure is underway.",
+    "color": "#FF5722"
+  },
+  {
+    "status": "Closing",
+    "message": "Surgery completed. The patient is being prepared for recovery.",
+    "color": "#00C853"
+  },
+  {
+    "status": "Recovery",
+    "message": "Patient transferred to post-surgery recovery room.",
+    "color": "#4CAF50"
+  },
+  {
+    "status": "Complete",
+    "message": "Recovery completed. Patient awaiting dismissal.",
+    "color": "#607D8B"
+  },
+  {
+    "status": "Dismissal",
+    "message": "Transferred to a hospital room for an overnight stay or for outpatient procedures the patient has left the hospital.",
+    "color": "#9C27B0"
+  }
+]
