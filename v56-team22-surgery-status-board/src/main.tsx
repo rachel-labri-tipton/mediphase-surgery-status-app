a10f9d3165bce35db33f15da543a3870ff9bbd01
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-08 18:08:52 +0200

    wrap router in main layout component

--- v56-team22-surgery-status-board/src/main.tsx
+++ v56-team22-surgery-status-board/src/main.tsx
@@ -2,33 +2,44 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router';
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router';
 import SignIn from './screens/SignIn.tsx';
 import PatientStatusBoard from './screens/PatientStatusBoard.tsx';
 import PatientInformation from './screens/PatientInformation.tsx';
 import PatientStatusUpdate from './screens/PatientStatusUpdate.tsx';
+import MainLayout from './layout/MainLayout.tsx';
+
+
+export const MainLayoutWrapper = () => (
+  <MainLayout>
+    <Outlet />
+  </MainLayout>
+);
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    Component: App,
-  },
-  {
-    path: '/sign-in',
-    Component: SignIn,
-  },
-  {
-    path: '/patient-status',
-    Component: PatientStatusBoard,
-  },
-  {
-    path: '/patient-information',
-    Component: PatientInformation,
-  },
-  {
-    path: '/update-patient',
-    Component: PatientStatusUpdate,
-  },
+    path: '/', Component: MainLayoutWrapper,
+    children: [
+      { index: true, Component: App },
+      {
+        path: '/sign-in',
+        Component: SignIn,
+      },
+      {
+        path: '/patient-status',
+        Component: PatientStatusBoard,
+      },
+      {
+        path: '/patient-information',
+        Component: PatientInformation,
+      },
+      {
+        path: '/update-patient',
+        Component: PatientStatusUpdate,
+      },
+    ],
+    }
 ]);
 
 createRoot(document.getElementById('root')!).render(
