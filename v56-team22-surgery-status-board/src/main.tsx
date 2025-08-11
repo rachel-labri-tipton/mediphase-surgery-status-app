36bfe6362825d08fc8873380e9ee4738c767a1e2
Manpreet Singh <ManpreetSL@users.noreply.github.com>
2025-08-11 14:54:19 +0100

    feat add Vercel Analytics

--- v56-team22-surgery-status-board/src/main.tsx
+++ v56-team22-surgery-status-board/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/next';
 import './index.css';
 import App from './App.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router';
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>
 );
