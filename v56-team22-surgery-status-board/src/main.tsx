82646f24a58b4bbc7a08a3da9cae702a9fbf5c56
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-12 12:42:15 +0200

    fix: move routes and MainLayoutWrapper into separate components cleanup: remove mainlayout from App.tsx cleanup: remove routes from main.tsx

--- v56-team22-surgery-status-board/src/main.tsx
+++ v56-team22-surgery-status-board/src/main.tsx
@@ -1,49 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter} from 'react-router';
+import { AuthProvider } from './contexts/AuthContext';
+import routes from './routes.ts';
 import './index.css';
-import App from './App.tsx';
-import { RouterProvider, createBrowserRouter, Outlet } from 'react-router';
-import SignIn from './screens/SignIn.tsx';
-import PatientStatusBoard from './screens/PatientStatusBoard.tsx';
-import PatientInformation from './screens/PatientInformation.tsx';
-import PatientStatusUpdate from './screens/PatientStatusUpdate.tsx';
-import MainLayout from './layout/MainLayout.tsx';
 
 
-export const MainLayoutWrapper = () => (
-  <MainLayout>
-    <Outlet />
-  </MainLayout>
-);
-
-
-const router = createBrowserRouter([
-  {
-    path: '/', Component: MainLayoutWrapper,
-    children: [
-      { index: true, Component: App },
-      {
-        path: '/sign-in',
-        Component: SignIn,
-      },
-      {
-        path: '/patient-status',
-        Component: PatientStatusBoard,
-      },
-      {
-        path: '/patient-information',
-        Component: PatientInformation,
-      },
-      {
-        path: '/update-patient',
-        Component: PatientStatusUpdate,
-      },
-    ],
-    }
-]);
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
