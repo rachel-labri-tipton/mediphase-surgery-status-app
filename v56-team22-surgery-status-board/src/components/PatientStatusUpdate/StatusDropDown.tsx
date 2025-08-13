704617cff9282fa5df42376b599e32f0f586afb3
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-13 14:23:04 +0200

    add status colors to StatusDropDownMenu

--- v56-team22-surgery-status-board/src/components/PatientStatusUpdate/StatusDropDown.tsx
+++ v56-team22-surgery-status-board/src/components/PatientStatusUpdate/StatusDropDown.tsx
@@ -5,47 +5,47 @@ import {
   DropdownMenuItem,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { statusLabels } from'@/constant/status-info'
+import { getStatusOptions, getStatusColor, getContrastTextColor } from '@/utility/patientHelpers';
 
 
 
-function getStatusOptions(current: string) {
-  const idx = statusLabels.indexOf(current);
-  const options: string[] = [];
-  if (idx === 0) options.push(statusLabels[idx+1])
-  if (idx > 0) options.push(statusLabels[idx - 1], statusLabels[idx+1]);
-  if (idx === statusLabels.length - 1) options.push(statusLabels[idx - 1]);
-  return options;
-}
 
 const StatusDropDown = ({
   value,
   onChange,
+  originalStatus,
+  disabled = false,
 }: {
   value: string;
-  onChange: (value: string) => void;
+    onChange: (value: string) => void;
+    originalStatus: string;
+    disabled?: boolean;
   }) => {
-  const options = getStatusOptions(value);
+    const options =
+    value !== originalStatus ? [value] : getStatusOptions(originalStatus);
+  
   return (
     <DropdownMenu>
       <Trigger className="w-full">
-        <Button variant="outline" className="w-full">
+        <Button variant="outline" className={`w-full ${getContrastTextColor(getStatusColor(value))}`} disabled={disabled} style={{ backgroundColor: getStatusColor(value) }}>
           {value || 'Select Status'}
         </Button>
       </Trigger>
-      <DropdownMenuContent>
+      {!disabled && (<DropdownMenuContent>
         {options.map((status) => (
           <DropdownMenuItem
             key={status}
             onSelect={() => {
-              onChange(status);
-              console.log(status);
+              onChange(status)
             }}
+            className={`flex justify-center items-center text-center my-2 cursor-pointer  w-full ${getContrastTextColor(getStatusColor(value))}`}
+            style={{ backgroundColor: getStatusColor(status) }}
           >
-            {status}
+              {status}
           </DropdownMenuItem>
         ))}
-      </DropdownMenuContent>
+      </DropdownMenuContent>)}
+      
     </DropdownMenu>
   );
 };
