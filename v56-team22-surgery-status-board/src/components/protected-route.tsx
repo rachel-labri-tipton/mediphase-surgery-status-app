bd5f71f6db6fc56b343834e445c8a308f02b298d
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-08-16 12:12:14 +0100

    feat: implement role-based routing

---
+++ v56-team22-surgery-status-board/src/components/protected-route.tsx
@@ -0,0 +1,34 @@
+import useAuth from '../hooks/useAuth';
+import type { Role } from '../constant/nav';
+import { Navigate } from 'react-router';
+
+interface ProtectedRouteProps {
+  allowedRoles?: Role[];
+  children: React.ReactNode;
+}
+const ProtectedRoute = ({ allowedRoles, children }: ProtectedRouteProps) => {
+  const { user } = useAuth();
+
+  console.log('ProtectedRoute user:', user);
+  //public route
+  if (allowedRoles === undefined) {
+    return <>{children}</>;
+  }
+  if (!user) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  if (user.role === 'admin') {
+    return <>{children}</>;
+  }
+  if (allowedRoles.length === 0) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to="/sign-in" replace />;
+  }
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
