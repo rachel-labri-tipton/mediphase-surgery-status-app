de8e25aab2098df0437fd1c09abb2fef911833c7
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-20 14:09:05 +0200

    create patient status board wrapper and add it to routes

---
+++ v56-team22-surgery-status-board/src/components/PatientStatusTable/PatientStatusBoardWrapper.tsx
@@ -0,0 +1,9 @@
+import useAuth from '@/hooks/useAuth';
+import PatientStatusBoard from '@/screens/PatientStatusBoard';
+
+const PatientStatusBoardWrapper = () => {
+  const { user } = useAuth();
+  return <PatientStatusBoard {...{ role: user?.role || 'guest' }} />;
+};
+
+export default PatientStatusBoardWrapper;
