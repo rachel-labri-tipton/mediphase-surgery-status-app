b2f7563681d6c33e2a97f0d95cc50d7f81c17de5
Rachel Tipton <rachelmtipton@gmail.com>
2025-08-25 12:30:58 +0200

    change project link to my repo

--- v56-team22-surgery-status-board/src/components/Footer/github-project-link.tsx
+++ v56-team22-surgery-status-board/src/components/Footer/github-project-link.tsx
@@ -4,7 +4,7 @@ const GithubProjectLink = () => {
   return (
     <div className="mt-8 text-center">
       <a
-        href="https://github.com/chingu-voyages/V56-tier2-team-22"
+        href="https://github.com/rachel-labri-tipton/mediphase-surgery-status-app"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center gap-2 px-4 py-2 text-lg font-semibold text-white bg-gray-900 rounded-full hover:bg-gray-900/80 transition-colors"
